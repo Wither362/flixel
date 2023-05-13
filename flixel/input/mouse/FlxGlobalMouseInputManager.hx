c047908733cd104f9433696261808c911e4faca2
Wither362 <93864752+Wither362@users.noreply.github.com>
2023-05-14 01:27:31 +0200

    Update FlxGlobalMouseInputManager.hx

--- flixel/input/mouse/FlxGlobalMouseInputManager.hx
+++ flixel/input/mouse/FlxGlobalMouseInputManager.hx
@@ -1,1 +1,22 @@
 package flixel.input.mouse;
+
+class FlxGlobalMouseInputManager {
+	public static var inUse:Bool = false;
+	public static var global:FlxBasic;
+
+	public static function mouseInput(of: FlxBasic):Bool
+	{
+		if(global != of && !inUse)
+		{
+			inUse = !inUse;
+			if(!inUse)
+			{
+				global = null;
+			}
+			else
+			{
+				global = of;
+			}
+		}
+	}
+}
